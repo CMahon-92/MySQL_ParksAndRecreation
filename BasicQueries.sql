deff8afbb219554e91e148a6268c7020af736b19
Carleena S. Mahon <71473354+CMahon-92@users.noreply.github.com>
2025-05-14 18:57:31 -0500

    SQL queries using ORDER BY and GROUP BY
    Created additional SQL queries to practice data sorting and aggregation.
    - Used ORDER BY to sort results by specific columns
    - Used GROUP BY to group data and apply aggregate functions like COUNT and SUM

--- BasicQueries.sql
+++ BasicQueries.sql
@@ -11,3 +11,18 @@ WHERE gender = 'Female'
 SELECT first_name, occupation, salary 
 FROM employee_salary
 WHERE salary >60000 AND salary < 85000;
+
+-- Using Group By Clause
+SELECT gender, AVG(age)
+FROM employee_demographics
+GROUP BY gender
+
+-- Using Order By Clause
+SELECT * 
+FROM employee_salary
+ORDER BY salary ASC;
+
+-- Using Order By Clause to order multiple fields
+SELECT *
+FROM employee_demographics
+ORDER BY gender, age DESC
